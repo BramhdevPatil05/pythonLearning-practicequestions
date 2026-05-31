5ca1104bab68bc7aac5564ce4e375334533953af
BramhdevPatil05 <bramhdevpatil5@gmail.com>
2026-05-31 08:55:18 +0530

    Add files via upload

---
+++ DAY_02_30TH MARCH.py
@@ -0,0 +1,16 @@
+
+
+# 7. Shopping Bill Generator
+
+# Take:
+
+# Product Name
+# Price
+
+
+product = input("Enter Product Name: ")
+price = int(input("Enter Price: "))
+
+print("Product:", product)
+print("Price: ₹" , price)
+
