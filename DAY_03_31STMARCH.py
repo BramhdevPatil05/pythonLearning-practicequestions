e04ebe5b03015631ee28f1513d84f6f62a27b7db
BRAMHDEV PATIL <bramhdevpatil5@gmail.com>
2026-06-09 14:16:33 +0530

    Update DAY_03_31STMARCH.py

---
+++ DAY_03_31STMARCH.py
@@ -0,0 +1,108 @@
+# Create two variables:
+# Swap their values without using a third variable.
+a = 10
+b = 20
+
+a,b = b,a
+print(a,b)
+
+
+
+# You have ₹1000 in your account. You deposit ₹500. What will the variable look like??
+
+balance = 1000
+
+balance = 1000 + 500
+
+print (balance)
+
+
+# You buy:
+
+# Shoes = ₹2000
+# T-shirt = ₹500
+
+# Store both prices and calculate the total.
+
+
+
+shoes = 2000
+tshirt = 500
+
+total = shoes + tshirt
+
+print(total)
+
+
+
+# An employee earns ₹50,000 and receives a ₹5,000 bonus.
+
+employee_salary = 50000
+bonus = 5000
+
+total_salary = employee_salary + bonus
+
+print(total_salary)
+
+
+# OPERATORS 
+
+
+# ARTHMETHIC OPERATORS
+
+
+burger = 120
+fries = 80
+
+total_bill = burger + fries
+
+total = 120*3 + 80*6
+
+total_billllll = 120/2555555 * 1805555555
+
+
+
+
+
+print(total_bill)
+
+print(total)
+
+print(total_billllll)
+
+# A pizza costs ₹300 and a drink costs ₹50. Calculate the total.
+
+
+pizza = 300 
+drink = 50
+
+total = pizza + drink
+
+print(total)
+
+# Check if a person aged 21 can vote.
+age = 21
+
+print(age >= 18)
+
+
+
+# Starting balance is ₹5000. Deposit ₹1000 using an assignment operator.\
+
+balance = 5000
+balance = 5000 + 1000
+
+print(balance)
+
+# Check if 15 is greater than 20.
+
+a = 15
+b = 20
+
+print(a>b)
+
+# Check if "apple" exists in ["apple", "banana", "orange"].
+
+a = ["apple", "banana", "orange"]
+
+print("apple" in a)
